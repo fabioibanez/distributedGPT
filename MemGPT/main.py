75aea1fdcdc9d23648a2185cdec7c191fb1ed2f6
fabio <fabioi@stanford.edu>
2024-04-27 15:26:06 -0700

    CMT: delete useless files for main merge

--- MemGPT/main.py
+++
@@ -1,6 +0,0 @@
-import typer
-
-typer.secho(
-    "Command `python main.py` no longer supported. Please run `memgpt run`. See https://memgpt.readme.io/docs/quickstart.",
-    fg=typer.colors.YELLOW,
-)
