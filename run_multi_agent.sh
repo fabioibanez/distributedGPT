6e88ab7b34c3b3fecc637548c18bb036bc7f3681
Fabio Ibanez <fabioibanez27@gmail.com>
2024-05-10 11:50:48 -0700

    CMT: updated script

--- run_multi_agent.sh
+++ run_multi_agent.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
-run_in_terminal() {
-    command="$1"
-    gnome-terminal --command="bash -c '$command; exec bash'"
-}
+cd distributedGPT
 
-run_in_terminal "python3 multiple_machines.py --type server" &
+python3 multiple_machines.py --type server &
 
 for i in {1..4}
 do
-    run_in_terminal "python3 multiple_machines.py --type client" &
+    python3 multiple_machines.py --type client &
 done
 
 wait
